e2933e0e5b92806e4f176b74c7f4aab0d0c0cfe7
matth <matthisf.etudes@gmail.com>
2026-06-12 09:35:06 +0200

    :building_construction: Modified architecture in microservices

---
+++ software-service/src/routes/post.route.js
@@ -0,0 +1,14 @@
+﻿const express = require("express");
+
+const router = express.Router();
+
+const AuthController = require("../controllers/post.controller");
+const {authenticate} = require("../middleware/post.middleware");
+
+router.post("/register",  AuthController.register);
+
+router.post("/login",  AuthController.login);
+
+router.get("/validate", authenticate, AuthController.validate);
+
+module.exports = router;
